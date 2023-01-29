ad9e264b756e8965fedaa21b4e3a6727496e399b
jeronimoek <jeronimoek@gmail.com>
2023-01-29 04:12:20 -0300

    remove unused helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,29 +1,5 @@
-import { ColorFormat } from "./enums";
 import * as vscode from "vscode";
 import { ColorTranslator } from "colortranslator";
-
-export function getColorType(color: string, hasAlpha: boolean): ColorFormat {
-  const start = color.split("(")[0];
-  let type: ColorFormat;
-  switch (start) {
-    case "rgb":
-    case "rgba":
-      type = hasAlpha ? ColorFormat.RGBA : ColorFormat.RGB;
-      break;
-    case "hsl":
-    case "hsla":
-      type = hasAlpha ? ColorFormat.HSLA : ColorFormat.HSL;
-      break;
-    case "device-cmyk":
-    case "cmyk":
-      type = hasAlpha ? ColorFormat.CMYKA : ColorFormat.CMYK;
-      break;
-    default:
-      type = hasAlpha ? ColorFormat.HEXA : ColorFormat.HEX;
-      break;
-  }
-  return type;
-}
 
 export function matchColors(text: string) {
   const colorRegex =
